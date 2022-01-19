9d76ceea91fda49602e2d59e1b7d41d09deaa518
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-01-19 05:00:52 -0600

    Initial Upload
    Moved files from another repository

---
+++ Clean-List.ps1
@@ -0,0 +1,48 @@
+﻿
+$computerNames = 'BLUSTRAA01
+  ___BLUSTRAA02
+  BLUSTRAA03
+  ___BLUSTRAA04
+  BLUSTRAA05
+  ___BLUSTRAA06
+  BLUSTRAA07
+  ___BLUSTRAA08
+  000BLUSTRAA09
+  ___0000CHIDIACA01
+  000CHIDIACA02
+  CHIDIACA03
+  000CHIDIACA04
+  CHIDIACA05
+  CHIDIACA06
+  CHIDIACA07
+  CHIDIACA08
+CHIDIACA09'.Replace("`n",',')
+
+#Fix a list of text 
+$ComputerList = $computerNames.Split(',').TrimStart('_','0',' ').Replace('CA','-').Replace('AA','-')
+
+
+#Make reusable
+function Optimize-List
+{
+  param(
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [String[]]$List
+  )
+  $CommaString = $List.Replace("`n",',')
+  $CleanArray = $CommaString.Split(',').Trim('_',',','.','\','/','-','@',' ').TrimStart('0')
+  Return $CleanArray
+}
+
+Optimize-List -List $computerNames
+
+
+#Convert it to a hashtable with FQDN's and use it to Ping those names (uses write-host instead of "Ping" for the example)
+$FQDNlist = @{} #Hashtable
+$ComputerList | ForEach-Object -Process {
+  $FQDNlist.$_ = $_.ToLower()+'.domain.com'
+}
+foreach($key in $FQDNlist.Keys)
+{
+  Write-Host -Object "Ping: $($FQDNlist.$key)" -ForegroundColor Yellow
+}
---
+++ Ping-IpRange.ps1
@@ -0,0 +1,27 @@
+﻿#Not really putting all together, but here are some things that you can do:
+for($LastOctet = 0;$LastOctet -lt 10;$LastOctet++)
+{
+  $ip = "192.168.0.$LastOctet"
+  Write-Host -Object "Ping: $ip -- Responds:  $(Test-Connection -ComputerName $ip -Count 1 -Quiet)"
+}
+
+#Or make it more reusable
+function Ping-IpRange
+{
+  param(
+    [Parameter(Mandatory,Position = 0)]
+    $First3Octets,
+    [Parameter(Mandatory,Position = 1)]
+    $FirstAddress,
+    [Parameter(Mandatory,Position = 2)]
+    $LastAddress
+  )
+  for($LastOctet = $FirstAddress;$LastOctet -lt $LastAddress;$LastOctet++)
+  {
+    $ip = "$First3Octets.$LastOctet"
+    Write-Host -Object "Ping: $ip -- Responds:  $(Test-Connection -ComputerName $ip -Count 1 -Quiet)"
+  }
+}
+
+Ping-IpRange -First3Octets 192.168.0 -FirstAddress 0 -LastAddress 10
+
