57b06a46d0050e913d54090cdb693003e9d5b48f
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-01-19 22:11:58 -0600

    Create ISEO.Tools.psm1

---
+++ ISEO.Tools.psm1
@@ -0,0 +1,26 @@
+﻿Clear-Host
+Write-Host "Loading module - ISEO.Tools " -NoNewline
+for($i=0;$i -lt 5;$i++){Sleep 1; Write-Host '. ' -NoNewline -ForegroundColor Green}
+
+function Ping-IpRange
+{
+  param(
+    [Parameter(Mandatory,Position = 0)]
+    $First3Octets,
+    [Parameter(Mandatory,Position = 1)]
+    $FirstAddress,
+    [Parameter(Mandatory,Position = 2)]
+    $LastAddress
+  )
+  for($LastOctet = $FirstAddress;$LastOctet -lt $LastAddress;$LastOctet++)
+  {
+    $ip = "$First3Octets.$LastOctet"
+    Write-Host -Object "Ping: $ip -- Responds:  $(Test-Connection -ComputerName $ip -Count 1 -Quiet)"
+  }
+}
+
+
+
+# At the very bottom of the module script type the following and save it.  This should always be the last line in your module.
+Export-ModuleMember Ping-IpRange
+
