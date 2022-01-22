8923636100aa60609d7e5a784e5d118922bfc67e
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-01-22 08:47:34 -0600

    Update Clean-List.ps1

--- Clean-List.ps1
+++ Clean-List.ps1
@@ -1,4 +1,4 @@
-﻿
+﻿# Create the list
 $computerNames = 'BLUSTRAA01
   ___BLUSTRAA02
   BLUSTRAA03
@@ -16,21 +16,25 @@ $computerNames = 'BLUSTRAA01
   CHIDIACA06
   CHIDIACA07
   CHIDIACA08
-CHIDIACA09'.Replace("`n",',')
+CHIDIACA09'
 
-#Fix a list of text 
-$ComputerList = $computerNames.Split(',').TrimStart('_','0',' ').Replace('CA','-').Replace('AA','-')
+# Convert it to an String
+$ComputerList = $computerNames.Replace("`n",',')
+
+# Split the results, then trim the "spaces,0's and '_'" off the front. Replace the "CA" with a "-" then do the same with "AA".   
+$ComputerList.Split(',').TrimStart('_','0',' ').Replace('CA','-').Replace('AA','-')
 
 
 #Make reusable
 function Optimize-List
 {
   param(
-    [Parameter(Mandatory,HelpMessage='As an Array', ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [Parameter(Mandatory,HelpMessage = 'As an Array', ValueFromPipeline, ValueFromPipelineByPropertyName)]
     [String[]]$List
   )
+  $trimmings = ('_',',','.','\','/','-','@',' ')
   $CommaString = $List.Replace("`n",',')
-  $CleanArray = $CommaString.Split(',').Trim('_',',','.','\','/','-','@',' ').TrimStart('0')
+  $CleanArray = $CommaString.Split(',').Trim($trimmings).TrimStart('0')
   Return $CleanArray
 }
 
@@ -44,5 +48,16 @@ $ComputerList | ForEach-Object -Process {
 }
 foreach($key in $FQDNlist.Keys)
 {
-  Write-Host -Object "Ping: $($FQDNlist.$key)" -ForegroundColor Yellow
+  Write-Host -Object "Ping: $($FQDNlist.$key) - " -ForegroundColor Cyan -NoNewline
+  try
+  {
+    $result = Test-Connection -ComputerName $($FQDNlist.$key) -Count 1 -Quiet -ErrorAction Stop
+    $color = 'Green'
+  }
+  catch
+  {
+    $result = 'False'
+    $color = 'Red'
+  }
+  Write-Host -Object "$result" -ForegroundColor $color
 }
