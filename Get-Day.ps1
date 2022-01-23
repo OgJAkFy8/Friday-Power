ececa489073c0c9af8a78590016ac6a300049e1b
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-01-22 19:03:10 -0600

    Create Get-Day.ps1

---
+++ Get-Day.ps1
@@ -0,0 +1,31 @@
+﻿#requires -Version 3.0
+function Get-Day 
+{
+  param
+  (
+    [Parameter(Mandatory,HelpMessage='Enter date. Ex: 7/4/1776')]
+    [String]$Date,
+    [Int]$NumYears = 5
+  )
+
+  $Sepr = '-'
+  $OutputFormat = '{0,-4} : {2,-11} : {1}'
+  
+  [Int]$Year = Get-Date -Date $Date -Format yyyy
+  $Day = Get-Date -Date $Date -Format dd
+  $Month = Get-Date -Date $Date -Format MM
+
+  Write-Output -InputObject ($OutputFormat -f 'Year', 'Julian', 'Day of Week')
+  Write-Output -InputObject ($OutputFormat -f $($Sepr*4), $($Sepr*6), $($Sepr*11))
+  for($i = 0;$i -lt $NumYears;$i++)
+  {
+    $Julian = Get-Date -Date $Date -UFormat %j
+    $dayofweek = (Get-Date -Date $Date).dayofweek
+  
+    Write-Output -InputObject ($OutputFormat -f $Year, $Julian, $dayofweek)
+    
+    $Year = $Year + 1
+    $Date = Get-Date -Date (('{0} / {1} / {2}' -f $Month, $Day, $Year))
+  }
+}
+
