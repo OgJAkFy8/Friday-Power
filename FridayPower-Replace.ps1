e915b6d5562a3709bef189e6742b082b01db5ed1
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-02-18 06:16:09 -0600

    Update FridayPower-Replace.ps1

--- FridayPower-Replace.ps1
+++ FridayPower-Replace.ps1
@@ -2,16 +2,30 @@
 Replace 
 #>
 
+# Simple replace
 'This_is_a_string_with_spaces.'.Replace('_', ' ')
 
+# You can put two "replace" statements in one line
+'This_is_a_string_with_spaces.'.Replace('_', ' ').Replace('string','sentence')
 
+
+# You can use the "replace" statement on files too
 $String = @"
 Line one
 Line two
 Line three
 "@
 
-$String | Out-File c:\temp\ReplaceFile.txt
-(Get-Content c:\temp\ReplaceFile.txt -Raw) -Replace ' ', '_' | Set-Content c:\temp\ReplaceFile.txt
+$String | Out-File c:\temp\ReplaceFile.txt # This makes our file
+Get-Content C:\temp\ReplaceFile.txt # This shows what is in it currently
 
-Get-Content c:\temp\ReplaceFile.txt 
+# To make changes to a file
+(Get-Content c:\temp\ReplaceFile.txt -Raw) -Replace 'Line', 'This is sentence' | Set-Content c:\temp\ReplaceFile.txt
+
+# To make changes to a file and create a new one.
+$Oldfile = (Get-Content c:\temp\ReplaceFile.txt -Raw) -Replace 'Line', 'This is sentence' 
+$Oldfile | Set-Content c:\temp\NewReplaceFile.txt
+
+# View the new file
+Get-Content c:\temp\NewReplaceFile.txt # This shows the changes
+
