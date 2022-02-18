d275a0b4bf6bb4c89d695b76a7a96244beafb8c7
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-02-17 21:35:09 -0600

    Create FridayPower-Replace.ps1

---
+++ FridayPower-Replace.ps1
@@ -0,0 +1,17 @@
+﻿<#
+Replace 
+#>
+
+'This_is_a_string_with_spaces.'.Replace('_', ' ')
+
+
+$String = @"
+Line one
+Line two
+Line three
+"@
+
+$String | Out-File c:\temp\ReplaceFile.txt
+(Get-Content c:\temp\ReplaceFile.txt -Raw) -Replace ' ', '_' | Set-Content c:\temp\ReplaceFile.txt
+
+Get-Content c:\temp\ReplaceFile.txt 
