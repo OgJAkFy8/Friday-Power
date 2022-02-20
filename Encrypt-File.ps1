4bae9d86d186b92af5f743355e58a0f6d9eb105f
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-02-20 06:28:20 -0600

    Create Encrypt-File.ps1

---
+++ Encrypt-File.ps1
@@ -0,0 +1,10 @@
+﻿if(-Not (Test-Path Cert:\CurrentUser\My\BC07CD0F1718E6F7F10A22E3C42585B14E6571ED)){
+  New-SelfSignedCertificate -DnsName MyDocumentEncryption -FriendlyName MyDocumentEncryption -CertStoreLocation "Cert:\CurrentUser\My" -KeyUsage KeyEncipherment,DataEncipherment, KeyAgreement -Type DocumentEncryptionCert
+}
+
+'SecretPassword!@#$' | Protect-CmsMessage -To CN=MyDocumentEncryption -OutFile .\Password1
+
+Get-CmsMessage -Path .\Password1
+Unprotect-CmsMessage -Path .\Password1
+
+
