fb7d55a9fcd98500fd6bc77deaeed71f00bd87d4
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-03-13 21:37:02 -0500

    Update FridayPower-DnsName.ps1

--- FridayPower-DnsName.ps1
+++ FridayPower-DnsName.ps1
@@ -1,4 +1,16 @@
 ﻿#requires -Version 2.0 -Modules DnsClient
+[CmdletBinding(SupportsShouldProcess = $true,ConfirmImpact = 'Low',DefaultParameterSetName = 'IpAddressList')]
+
+param
+(
+  [Parameter(Mandatory = $true,HelpMessage = 'Must be an Array of Ip Addresses', ValueFromPipeline = $true,
+  ValueFromPipelineByPropertyName = $true,Position = 0,ParameterSetName = 'IpAddressList')]
+  [Object]$IpAddressList,
+  [Parameter(Mandatory = $true,ValueFromPipeline = $false,
+  ValueFromPipelineByPropertyName = $true,HelpMessage = 'Hashtable with DnsNames and IpAddresses',ParameterSetName = 'DnsHashTable')]
+  [Object]$DnsHashTable
+)
+#$IpAddressList
 <#
     .SYNOPSIS
     Checks a list of Hostnames against Ip Addresses
@@ -22,35 +34,40 @@
     }
 #>
 
-###################################################################
-#Edit this hashtable
-$DnsHashTable = @{
-  buffalo      = '192.168.0.59'
-  'printer.blue' = '192.168.0.59'
-}
-###################################################################
+<###################################################################
+    #Edit this hashtable
+    $DnsHashTable = @{
+    buffalo = '192.168.0.59'
+    pbworks = '208.96.18.237'
+    }
+###################################################################>
 
 #Do not change below.  Any modifications will change the results.  
-function Test-DnsSettings
+begin
+{
+function Compare-DnsNameToIp
 {
   <#
       .SYNOPSIS
       Checks a list of Hostnames against Ip Addresses
   #>
-  [CmdletBinding()]
+  [CmdletBinding(SupportsShouldProcess = $true,ConfirmImpact = 'Low')]
+
   param
   (
-    [Parameter(Mandatory = $true, Position = 0)]
+    [Parameter(Mandatory = $true,HelpMessage = 'Must be a hashtable.  See Help for example', ValueFromPipeline = $true,
+    ValueFromPipelineByPropertyName = $true,Position = 0)]
     [Object]$DnsHashTable
   )
   
+
   foreach($KeyName in $DnsHashTable.Keys)
   {
     Write-Host -Object ('{2}Expected: {2}{0} - {1}' -f $KeyName, $($DnsHashTable.$KeyName), "`n")
     try
     {
       # Test the Ip and return a hostname
-      $resolveName = (Resolve-DnsName -Name $($DnsHashTable.$KeyName) -DnsOnly -NoHostsFile -Server 192.168.0.3 -ErrorAction Stop).NameHost
+      $resolveName = (Resolve-DnsName -Name $($DnsHashTable.$KeyName) -DnsOnly -NoHostsFile -ErrorAction Stop).NameHost
     }
     catch
     {
@@ -66,9 +83,9 @@ function Test-DnsSettings
       
       try
       {
-        $resolveIp = (Resolve-DnsName -Name $KeyName -Type A -DnsOnly -NoHostsFile -Server 192.168.0.3 -ErrorAction Stop).NameHost
+        $resolveIp = (Resolve-DnsName -Name $KeyName -Type A -DnsOnly -NoHostsFile -ErrorAction Stop).NameHost
       
-        $resolveIp
+        $resolveIp.GetType().Name
       }
       catch
       {
@@ -78,6 +95,53 @@ function Test-DnsSettings
     }
   }
 }
+function Request-DnsNameFromIp
+{
+  [CmdletBinding(SupportsShouldProcess = $true,ConfirmImpact = 'Low')]
 
-Test-DnsSettings -DnsHashTable $DnsHashTable
+  param
+  (
+    [Parameter(Mandatory = $true,HelpMessage = 'Must be an Array of Ip Addresses', ValueFromPipeline = $true,
+    ValueFromPipelineByPropertyName = $true,Position = 0)]
+    [Object]$IpAddressList
+  )
+  BEGIN{
+    $MsgNotFound = 'Not Found'
+  }
+  PROCESS{  foreach($IpAddress in $IpAddressList)
+    {
+      Write-Host -Object ('{1}Testing: {1}{0} - ' -f $IpAddress, "`n") -NoNewline
+      try
+      {
+        # Test the Ip and return a hostname
+        $resolveName = (Resolve-DnsName -Name $($IpAddress) -DnsOnly -NoHostsFile -ErrorAction Stop).NameHost
+      }
+      catch
+      {
+        $resolveName = $MsgNotFound
+      }
+      if($resolveName -ne $MsgNotFound)
+      {
+        Write-Host -Object ('{0}' -f $resolveName) -ForegroundColor Green
+      }
+      else
+      {
+        Write-Host -Object ('{0}' -f $resolveName) -ForegroundColor Red
+      }
+    }
+  }
+  END{}
+}
+}
+process{
+if($IpAddressList)
+{
+  Request-DnsNameFromIp -IpAddressList $IpAddressList
+}
+if($DnsHashTable)
+{
+  Compare-DnsNameToIp -DnsHashTable $DnsHashTable
+}
+}
 
+END{}
