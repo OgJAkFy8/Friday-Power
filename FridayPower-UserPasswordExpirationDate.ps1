9bcc7b00fc7161cc8b3354b7f915ff26e895815f
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-03-14 21:30:17 -0500

    Create FridayPower-UserPasswordExpirationDate.ps1

---
+++ FridayPower-UserPasswordExpirationDate.ps1
@@ -0,0 +1,42 @@
+﻿function Get-UserPasswordExpirationDate
+{
+  <#
+      .SYNOPSIS
+      User Password Expiration Date
+  #>
+
+  [CmdletBinding(SupportsShouldProcess = $true,ConfirmImpact = 'Low')]
+
+  param
+  (
+    [Parameter(Mandatory = $true,HelpMessage = 'Must be an AD username', ValueFromPipeline = $true,
+    ValueFromPipelineByPropertyName = $true,Position = 0)]
+    [String]$AdUser
+  )
+
+  $userInfo = Get-ADUser $AdUser -Properties *
+  if($($userInfo.PasswordNeverExpires) -eq $false)
+  {
+    $userInfo |
+    Select-Object -Property DisplayName, msDS-UserPasswordExpiryTimeComputed |
+    Select-Object -Property 'Displayname', @{
+      Name       = 'ExpiryDate'
+      Expression = {
+        [datetime]::FromFileTime($_.'msDS-UserPasswordExpiryTimeComputed')
+      }
+    }
+  }
+  Else
+  {
+    $userInfo | Select-Object -Property DisplayName, PasswordNeverExpires
+  }
+}
+
+<# Doubleliner to get all users who have passwordneverexpires set to false and display the dates their passwords expire.
+    Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} -Properties DisplayName, msDS-UserPasswordExpiryTimeComputed |
+    Select-Object -Property 'Displayname', @{Name = 'ExpiryDate'; Expression = {[datetime]::FromFileTime($_.'msDS-UserPasswordExpiryTimeComputed')}}
+#>
+
+'one', 'two', 'three', 'four' | ForEach-Object -Process {
+  Get-UserPasswordExpirationDate -AdUser $_
+}
