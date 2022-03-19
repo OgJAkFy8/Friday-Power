bac0b442f9ecca3f71d6457ff18ec05743bdb0ef
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-03-19 08:39:28 -0500

    Update Encrypt-File.ps1

--- Encrypt-File.ps1
+++ Encrypt-File.ps1
@@ -1,5 +1,5 @@
-﻿if(-Not (Test-Path Cert:\CurrentUser\My\BC07CD0F1718E6F7F10A22E3C42585B14E6571ED)){
-  New-SelfSignedCertificate -DnsName MyDocumentEncryption -FriendlyName MyDocumentEncryption -CertStoreLocation "Cert:\CurrentUser\My" -KeyUsage KeyEncipherment,DataEncipherment, KeyAgreement -Type DocumentEncryptionCert
+﻿if(-Not (Test-Path -Path Cert:\CurrentUser\My\BC07CD0F1718E6F7F10A22E3C42585B14E6571ED)){
+  New-SelfSignedCertificate -DnsName MyDocumentEncryption -FriendlyName MyDocumentEncryption -CertStoreLocation 'Cert:\CurrentUser\My' -KeyUsage KeyEncipherment,DataEncipherment, KeyAgreement -Type DocumentEncryptionCert
 }
 
 'SecretPassword!@#$' | Protect-CmsMessage -To CN=MyDocumentEncryption -OutFile .\Password1
@@ -8,8 +8,3 @@ Get-CmsMessage -Path .\Password1
 Unprotect-CmsMessage -Path .\Password1
 
 
-# save PSCredential in the file
-$credential = Get-Credential
-$credential | Export-CliXml -Path ".\myCred_${env:USERNAME}_${env:COMPUTERNAME}.xml"
-
-$credential = Import-CliXml -Path ".\myCred_${env:USERNAME}_${env:COMPUTERNAME}.xml"
