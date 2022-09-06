f18b1d1f6eeda6cfec8986a2cdf6b10f2e9cdcb3
Erik JA <OgJAkFy8@users.noreply.github.com>
2022-09-05 20:54:10 -0500

    Create ModuleLoader.psm1

---
+++ ModuleLoader.psm1
@@ -0,0 +1,10 @@
+﻿$Functionpath = $PSScriptRoot + '\Scripts\'
+
+$FunctionList = Get-ChildItem -Path $Functionpath -Name -File
+
+foreach($function in $FunctionList)
+{
+  . ($Functionpath + $function)
+}
+
+#(Get-Command -Module OMCS.Tools).Name
